8d26d7cbb74065d20ed17e0e848d51a798ee7d65
Karen <136932490+GabsNandes@users.noreply.github.com>
2024-08-22 15:10:46 -0300

    added teleport

---
+++ Assets/teleportationManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class teleportationManager : MonoBehaviour
+{
+
+    [SerializeField] private InputActionAsset actionAsset;
+    [SerializeField] private XRRayInteractor rayInteractor;
+    [SerializeField] private TeleportationProvider provider;
+    private InputAction _thumbstick;
+    private bool _isActive;
+
+    void Start()
+    {
+        rayInteractor.enabled = false;
+        
+        var activate = actionAsset.FindActionMap("XRI LeftHand").FindAction("Teleport Mode Activate");
+        activate.Enable();
+        activate.performed += OnTeleportActivate;
+
+        var cancel = actionAsset.FindActionMap("XRI LeftHand").FindAction("Teleport Mode Cancel");
+        cancel.Enable();
+        cancel.performed += OnTeleportCancel;
+        
+        _thumbstick = actionAsset.FindActionMap("XRI LeftHand").FindAction("Move");
+        _thumbstick.Enable();
+    }
+
+    void Update()
+    {
+        if (!_isActive)
+            return;
+
+        if (_thumbstick.triggered)
+            return;
+
+        if (!rayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit hit))
+        {
+            rayInteractor.enabled = false;
+            _isActive = false;
+            return;
+        }
+
+        TeleportRequest request = new TeleportRequest()
+        {
+            destinationPosition = hit.point,
+            // destinationRotation = ?,
+        };
+
+        provider.QueueTeleportRequest(request);
+        
+        rayInteractor.enabled = false;
+        _isActive = false;
+    }
+
+    private void OnTeleportActivate(InputAction.CallbackContext context)
+    {
+        rayInteractor.enabled = true;
+        _isActive = true;
+    }
+
+    private void OnTeleportCancel(InputAction.CallbackContext context)
+    {
+        rayInteractor.enabled = false;
+        _isActive = false;
+    }
+}
